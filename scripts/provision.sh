df8e4e911e9479a5291ca2d14284186b0d3d7c2e
Ady Romantika <ady.romantika@gmail.com>
2016-08-19 16:30:41 +0800

    Add ansible playbooks and provisioning script

---
+++ scripts/provision.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+# Make sure that this script can be run from any directory
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# SSH key file
+SSH_KEY_FILENAME=automate.pem
+SSH_KEY=${SCRIPTDIR}/../${SSH_KEY_FILENAME}
+
+if [ ! -f ${SSH_KEY} ]; then
+    echo "Error: The file ${SSH_KEY} can't be found"
+    exit 1
+else
+    chmod 600 ${SSH_KEY}
+fi
+
+if [ -z ${AWS_ACCESS_KEY_ID} ] || [ -z ${AWS_SECRET_ACCESS_KEY} ]; then
+    echo "Error: Both AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environtment variables are required"
+    exit 1
+fi
+
+# Load identity into agent
+ssh-add ${SSH_KEY}
+ssh-add -l
+
+# Launch the automation
+cd ${SCRIPTDIR}/../ansible
+ansible-playbook launch.yml -vv
+
